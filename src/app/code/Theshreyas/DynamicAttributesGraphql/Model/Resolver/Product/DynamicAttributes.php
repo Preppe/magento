d6d251de2e69317ea1e51ddca1c00d8a952fe2c1
Giuseppe Fumarola <preppe21@gmail.com>
2022-07-19 18:09:15 +0200

    Revert "prova a crudo del dynamic attribute"
    This reverts commit f2cdcec44efe1f53de83a8405cba1a77b0e91913.

--- src/app/code/Theshreyas/DynamicAttributesGraphql/Model/Resolver/Product/DynamicAttributes.php
+++ src/app/code/Theshreyas/DynamicAttributesGraphql/Model/Resolver/Product/DynamicAttributes.php
@@ -44,6 +44,6 @@ class DynamicAttributes implements ResolverInterface
         $data[$fi] = $product->getData($fi);
       }
     }
-    return $data;
+    return json_encode((object) $data);
   }
 }
