09a7e0b659444c38f41c05d8850f2de670e1e7c9
Giuseppe Fumarola <preppe21@gmail.com>
2022-12-29 21:10:42 +0100

    clinotty

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 bin/start --no-dev
 bin/copytocontainer --all
-bin/composer install
-bin/magento setup:upgrade
-bin/magento deploy:mode:set production
-bin/magento indexer:reindex
-bin/magento maintenance:disable
+bin/clinotty composer install
+bin/clinotty bin/magento setup:upgrade
+bin/clinotty bin/magento deploy:mode:set production
+bin/clinotty bin/magento indexer:reindex
+bin/clinotty bin/magento maintenance:disable
